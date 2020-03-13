b69a7899c29f659abbc590cffb476c809916b38e
ztnew <ztnew@qq.com>
2020-03-13 10:40:30 +0800

    refact the sec new item

--- functional_tests.py
+++ functional_tests.py
@@ -12,6 +12,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self,row_text):
+        table=self.browser.find_element_by_id('id_list_table')
+        rows=table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text,[row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         #伊迪丝听说有一个很酷的在线待办事项应用
         #他去看了这个应用的首页
@@ -22,14 +27,11 @@ class NewVisitorTest(unittest.TestCase):
         header_text=self.browser.find_element_by_tag_name('h1').text
         self.assertIn( 'To-Do' ,header_text)
         
-        
         #应用邀请她输入一个待办事项
         inputbox=self.browser.find_element_by_id('id_new_item')
         self.assertEqual(
         inputbox.get_attribute('placeholder'),
         'Enter a to-do item')
-        
-
 
         #她在一个文本框中输入了“Buy peacock feathers” (购买孔雀羽毛)
         #伊迪丝的爱好是使用假蝇做饵钓鱼
@@ -38,26 +40,25 @@ class NewVisitorTest(unittest.TestCase):
         #她按回车键后，页面更新了
         #待办事项表格中显示了"1: Buy peacock feathers"
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
+        time.sleep(1)        
+        self.check_for_row_in_list_table('1:Buy peacock feathers')
         
-
-        table=self.browser.find_element_by_id('id_list_table')
-        rows=table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text=='1:Buy peacock feathers' for row in rows)，
-            "New to-do item did not appear in table"
-            )
         #页面又显示了一个文本框，可以输入其他的待办事项
         #她输入了“Use peacock feathers to make a fly”(使用孔雀羽毛做假蝇)
         #伊迪丝做事很有条理
-        self.fail('Finish the test!')
-        
+        inputbox=self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)   
 
         #页面再次更新，他的清单中显示了两个待办事项
+        self.check_for_row_in_list_table('1:Buy peacock feathers')
+        self.check_for_row_in_list_table('2:Use peacock feathers to make a fly')
 
         #伊迪丝想知道这个网站是否会记住他的清单
         #他看到网站为他生成了一个唯一的URL
         #而且页面中有一些文字解说这个功能
+        self.fail('Finish the test!')
 
         #她访问那个URL,发现她的待办事项列表还在
 
