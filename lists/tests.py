792e59e509893e8398e9c3e7744fa467e1c51ee1
ztnew <ztnew@qq.com>
2020-03-17 12:47:07 +0800

    new URL + view for adding to existing lists.FT passes :-)

--- lists/tests.py
+++ lists/tests.py
@@ -98,7 +98,15 @@ class NewItemTest(TestCase):
 
         self.assertRedirects(response,f'/lists/{correct_list.id}/')
         
-        
+    def test_passes_correct_list_to_template(self):
+        other_list=List.objects.create()
+        correct_list=List.objects.create()
+
+        response= self.client.get(f'/lists/{correct_list.id}/')
+
+        self.assertEqual(response.context['list'],correct_list)
+    
+    
         
     
     
